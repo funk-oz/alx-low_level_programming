7d332bf93b8ef1f5d39fbb663c4b1838ac277f24
funk-oz <kieranmarvin70@gmail.com>
2022-06-30 14:09:40 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 /**
- *
  *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
  *   * Return: 0 if exited properly, non-zero otherwise
@@ -11,9 +9,6 @@
 int main(void)
 
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
