dfb26e61d551f101bb06992fa4b96c33a82d1a62
funk-oz <kieranmarvin70@gmail.com>
2022-07-08 03:45:41 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		if ((i == 2) || (i == 4))
 			continue;
-		else
-			_putchar(i + '0');
+	else
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
